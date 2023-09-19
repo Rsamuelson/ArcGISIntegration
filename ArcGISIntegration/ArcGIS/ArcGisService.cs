a85a42dbe23c7d9a4c33f2a361264d73929a48a4
Ryan Samuelson <36461437+Rsamuelson@users.noreply.github.com>
2023-09-19 12:11:37 -0500

    Added ArcGIS Integration sample

---
+++ ArcGISIntegration/ArcGIS/ArcGisService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+
+namespace ArcGIS;
+
+/// <summary>
+///     Integration service for ArcGIS.
+///     See: https://developers.arcgis.com/
+/// </summary>
+public sealed class ArcGisService
+{
+    private readonly HttpClient _arcGisHttpClient;
+
+    public ArcGisService(string referrer)
+    {
+        _arcGisHttpClient = new HttpClient();
+
+        _arcGisHttpClient.DefaultRequestHeaders.Add(HeaderNames.Referer, referrer);
+    }
+
+    /// <summary>
+    ///     Calls the following API endpoint and returns the top scoring candidate.
+    ///     https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?{address}&f=json&token={token}
+    ///     See: https://developers.arcgis.com/documentation/mapping-apis-and-services/geocoding/geocode-addresses/
+    /// </summary>
+    public async Task<string> GetAddressLocationCoordinatesAsync(string address, string token, CancellationToken cancellationToken = default)
+    {
+        var queryUri = "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates";
+
+        var queryStringKeyValuePairs = new Dictionary<string, string>()
+        {
+            { "f", "json" },
+            { "token", token },
+            { "address", address }
+        };
+        queryUri = QueryHelpers.AddQueryString(queryUri, queryStringKeyValuePairs);
+
+        var httpResponseMessage = await _arcGisHttpClient.GetAsync(queryUri, cancellationToken);
+
+        var contentString = await httpResponseMessage.Content.ReadAsStringAsync(cancellationToken);
+
+        return contentString;
+
+        //In our actual code we Deserialize the contextString with json and return top scoring the result, but for testing we don't need to do that.
+    }
+}
---
+++ ArcGISIntegration/ArcGIS/Program.cs
@@ -0,0 +1,24 @@
+﻿using ArcGIS;
+
+//Note for whoever this may concern:
+//   When we were initially testing out the Api Key/Referrer functionality, we were not getting a error code 498 on the invalid referrer call.
+//   Afterword, when we opened up a ticket with ESRI and when through the trouble shooting process we found that our called were now being blocked based on the referrer, which is what we initially wanted.
+//   From our side, we are considering this issue closed because we cannot reproduce the issue.
+
+var token = "[token]"; //TODO: Set token/Api Key
+var validReferrer = "[referrer]"; //TODO: set valid referrer
+var invalidReferrer = "abcd1234";
+
+var validAddress = "2309 Euclid Ave, Lower Beaver, Des Moines, 50310";
+
+//Valid Referrer
+var expectedValidResult = await new ArcGisService(validReferrer).GetAddressLocationCoordinatesAsync(validAddress, token); 
+Console.WriteLine("Valid referrer result:");
+Console.WriteLine(expectedValidResult);
+Console.WriteLine();
+
+//Invalid Referrer
+var expectedInvalidResult = await new ArcGisService(invalidReferrer).GetAddressLocationCoordinatesAsync(validAddress, token);
+Console.WriteLine("Invalid referrer result:");
+Console.WriteLine(expectedInvalidResult);
+
